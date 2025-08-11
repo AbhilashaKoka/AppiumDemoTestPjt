aee19d7474759bd0119e7b315fe2833a8c906e0e
AbhilashaKoka <kokaabhilasha@rediffmail.com>
2025-08-11 20:09:51 +0530

    11082025-02

--- src/test/java/com/hybridmobtest/BrowserBaseSetUp.java
+++ src/test/java/com/hybridmobtest/BrowserBaseSetUp.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
+
+import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -42,24 +44,22 @@ public class BrowserBaseSetUp {
 	}
 
 
-	private DesiredCapabilities setCapabilitiesForAndroid() {
-		try {
-			service = AppiumDriverLocalService.buildDefaultService();
-			DesiredCapabilities cap = new DesiredCapabilities();
-			cap.setCapability("deviceName", "emulator-5554");
-			cap.setCapability("platformName", "Android");
-			cap.setCapability("platformVersion", "10");
-			cap.setCapability("automationName", "UIAutomator2");
-			cap.setCapability("browserName", "CHROME");
-			cap.setCapability("appium:ChromeOptions", ImmutableMap.of("w3c", false));
-			ChromeOptions chromeOptions = new ChromeOptions();
-			chromeOptions.setExperimentalOption("w3c", false);
-			cap.merge(cap);
-			return cap;
-		} catch (Exception ex) {
-			throw new IllegalArgumentException("Browser isn't supported.");
-		}
-	}
+    private DesiredCapabilities setCapabilitiesForAndroid() {
+        try {
+            DesiredCapabilities cap = new DesiredCapabilities();
+            cap.setCapability("deviceName", "emulator-5554");
+            cap.setCapability("platformName", "Android");
+            cap.setCapability("platformVersion", "10");
+            cap.setCapability("automationName", "UIAutomator2");
+            cap.setCapability("browserName", "Chromium");
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.setExperimentalOption("w3c", false);
+            cap.setCapability("appium:chromeOptions", chromeOptions);
+            return cap;
+        } catch (Exception ex) {
+            throw new IllegalArgumentException("Browser isn't supported.");
+        }
+    }
 
 	public static void execKill(long minutes) throws InterruptedException {
 		try {
@@ -70,14 +70,30 @@ public class BrowserBaseSetUp {
 		}
 	}
 
-	public void startServer() {
-		service = new AppiumServiceBuilder()
-				.withArgument(() -> "--use-drivers", "uiautomator2,chromium")
-				.withArgument(() -> "--use-plugins", "execute-driver") // Custom argument for plugins
-//				.usingAnyFreePort()
-				.build();
-		service.start();
-	}
+    public void startServer() {
+        service = new AppiumServiceBuilder()
+                .withIPAddress("0.0.0.0")
+                .usingPort(4723)
+                .withArgument(() -> "--use-drivers", "uiautomator2,chromium")
+                .withArgument(() -> "--use-plugins", "execute-driver")
+                .build();
+        service.start();
+    }
+
+
+
+//    public void startServer() {
+//        service = new AppiumServiceBuilder()
+//                .usingDriverExecutable(new File("C:\\Program Files\\nodejs\\node.exe"))
+//                .withAppiumJS(new File("C:\\ApplicationPath\\appium\\appium"))
+//                 .withIPAddress("127.0.0.1")
+//                .usingPort(4723)
+//                .withArgument(() -> "--use-drivers", "uiautomator2,chromium")
+//                .withArgument(() -> "--use-plugins", "execute-driver")
+//                .withLogFile(new File("AppiumLog.txt"))
+//                .build();
+//        service.start();
+//    }
 
 	public void stopServer() {
 		if (service != null) {
