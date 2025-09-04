325e93059d9cb26ec2266a2bdbd38253eaf2b0d0
AbhilashaKoka <kokaabhilasha@rediffmail.com>
2025-09-04 16:35:27 +0530

    04092025-02

--- src/test/java/com/appiumGridTest/GridTest.java
+++ src/test/java/com/appiumGridTest/GridTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.*;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 
@@ -27,6 +28,15 @@ public class GridTest extends GridLauncher2 {
 //        capabilites.setPlatformVersion("10");
 //        capabilites.setChromedriverExecutable("C:\\Users\\Abhilasha\\Documents\\DOCUMENT\\StudyDocumentFolder\\IDE\\APPIUMSetUp\\drivers\\chromedriver_74\\chromedriver.exe");
 //        capabilites.setNoReset(true);
+        AndroidDriver driver = getAndroidDriver();
+        System.out.println("Driver initialized successfully: " + driver.getCapabilities().getBrowserName());
+        driver.get("https://www.google.com");
+        driver.findElement(By.name("q")).sendKeys("Automation");
+        driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+        driver.quit();
+        }
+
+    private static AndroidDriver getAndroidDriver() throws MalformedURLException {
         DesiredCapabilities caps = new DesiredCapabilities();
         caps.setCapability("platformName", "android");
         caps.setCapability("deviceName", "emulator-5554");
@@ -41,16 +51,11 @@ public class GridTest extends GridLauncher2 {
         // Point to Selenium Grid hub, which routes to Appium node
         URL gridUrl = new URL("http://192.168.1.3:4444");
         AndroidDriver driver = new AndroidDriver(gridUrl, caps);
-        System.out.println("Driver initialized successfully: " + driver.getCapabilities().getBrowserName());
-        driver.get("https://www.google.com");
-        driver.findElement(By.name("q")).sendKeys("Automation");
-        driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
-        driver.quit();
-        }
+        return driver;
+    }
 
 
-
-        @AfterTest
+    @AfterTest
          public void tearDown() throws IOException {
 //            killExistingJavaProcesses();
             System.out.println("Test completed.");
